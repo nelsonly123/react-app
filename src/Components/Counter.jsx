8bad61c62074e77b6edd7c01684983cf8b28ac04
unknown <nelsonly123@gmail.com>
2023-07-06 15:52:28 -0700

    Added hold down increment/decrement counter feature

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,7 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export default function Counter({ id, handleRemoveSingleCounter}) {
     const [count, setCount] = useState(0);
+    const intervalRef = useRef(null);
+
+    useEffect(() => {
+        return () => stopCounter();
+    }, []);
 
     const incrementCounter = () => {
         setCount((prevCount) => prevCount + 1)
@@ -13,18 +18,47 @@ export default function Counter({ id, handleRemoveSingleCounter}) {
         setCount(0);
     }
 
+    const increaseCounter = () => {
+        if(intervalRef.current) return;
+        intervalRef.current = setInterval(() => {
+            setCount((prevCounter) => prevCounter + 1)
+        }, 60);
+    }
+
+    const decreaseCounter = () => {
+        if(intervalRef.current) return;
+        intervalRef.current = setInterval(() => {
+            setCount((prevCounter) => prevCounter - 1)
+        }, 60);
+    }
+    
+    const stopCounter = () => {
+        if(intervalRef.current) {
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
+    }
+
     return (
         <div className='flex justify-center'>
             <div className='flex justify-between bg-indigo-200 w-64'>
                 <button 
                     className="h-10 px-5 m-3 text-blue-100 transition-colors duration-150 bg-blue-600 rounded-lg focus:shadow-outline hover:bg-blue-700"
-                    onClick={() => incrementCounter(count)}>
+                    onClick={() => incrementCounter(count)}
+                    onMouseDown={() => increaseCounter(count)}
+                    onMouseUp={() => stopCounter(count)}
+                    onMouseLeave={stopCounter}
+                    >
                         +
                 </button>
                 <div className="flex items-center text-lg">Count is {count}</div>
                 <button 
                     className="h-10 px-5 m-3 text-blue-100 transition-colors duration-150 bg-blue-600 rounded-lg focus:shadow-outline hover:bg-blue-700"
-                    onClick={() => decrementCounter(count)}>
+                    onClick={() => decrementCounter(count)}
+                    onMouseDown={() => decreaseCounter(count)}
+                    onMouseUp={() => stopCounter(count)}
+                    onMouseLeave={stopCounter}
+                    >
                         -
                 </button>
             </div>
@@ -38,7 +72,7 @@ export default function Counter({ id, handleRemoveSingleCounter}) {
                     className="h-10 px-5 m-3 text-blue-100 transition-colors duration-150 bg-blue-600 rounded-lg focus:shadow-outline hover:bg-blue-700" 
                     onClick={() => handleRemoveSingleCounter(id)}
                     >
-                        delete counter
+                        remove counter
                 </button>
             </div>
         </div>
