9d4bd0c8a67f0688545315f51ac884e25e348200
unknown <nelsonly123@gmail.com>
2024-05-25 17:51:57 -0700

    added DNR message

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -78,7 +78,6 @@ export default function Counter({ id, handleRemoveSingleCounter}) {
                     > remove counter
                 </button>
             </div>
-            <>Testing commit</>
         </div>
     )
 }
